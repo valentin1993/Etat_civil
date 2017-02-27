bc18da4a0ad9d667f62cb0735d4a0b951954f197
valentin1993 <valentin@datasystems.fr>
2017-02-27 16:53:38 +0100

    MAJ 27/02/2107

--- log/views.py
+++ log/views.py
@@ -3,6 +3,7 @@
 from django.contrib.auth import authenticate, login, logout
 from .forms import ConnexionForm
 from django.shortcuts import render, reverse, get_object_or_404, redirect
+from django.http import HttpResponseRedirect, HttpResponse
 
 
 
@@ -17,6 +18,7 @@ def connexion(request):
             user = authenticate(username=username, password=password)  # Nous vérifions si les données sont correctes
             if user:  # Si l'objet renvoyé n'est pas None
                 login(request, user)  # nous connectons l'utilisateur
+                return HttpResponseRedirect(reverse('accueil'))
             else: # sinon une erreur sera affichée
                 error = True
     else:
